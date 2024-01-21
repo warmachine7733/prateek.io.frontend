2a89cbb45af207dda450c8e21686da36b6f526ef
prateek jena <prateekjena@prateeks-MacBook-Air.local>
2024-01-21 20:06:28 +0530

    whoami added every 9 seconds

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,49 +1,38 @@
-import React from "react";
-// import Particles from "react-particles-js";
-import Snowfall from 'react-snowfall'
+import React, { useEffect, useState } from "react";
+import Snowfall from "react-snowfall";
 import "./index.css";
 
+const initialState = {
+  whoami: ["Web Developer", "Linux Enthusiast", "PC Gamer"],
+};
 
-export class Intro extends React.Component {
-  state = {
-    whoami: ["Web Developer", "Linux Enthusiast", "PC Gamer"],
-  };
-  setCurrentRole = () => {
-    //chnage in odd and even months
-    const currentMonth = new Date().getMonth()
-    if(currentMonth%2===0){
-      this.setState({index:0})
-    }else{
-      this.setState({index:1})
+export const Intro = (props) => {
+  const [intro, setIntro] = useState(initialState.whoami[0]);
+  useEffect(() => {
+    introHandler();
+    return () => {
+      clearTimeout(introHandler());
+    };
+  }, []);
+  const introHandler = () => {
+    let currentIndex = 0;
 
+    function printRole() {
+      setIntro(initialState.whoami[currentIndex]);
+      currentIndex = (currentIndex + 1) % initialState.whoami.length;
     }
-
-  // console.log("ok month",new Date().getMonth())
-  }
-  componentDidMount() {
-    this.setCurrentRole();
-  }
-
-  render() {
-    // console.log("props",this.props)
-    return (
-      <>
-      <div className="intro" style={{ height: this.props.availHeight }}>
-      <Snowfall/>
-        <div
-          className="introImage"
-          style={{ height: this.props.availHeight }}
-          // params={this.props.particles[this.state.index]}
-                  />
-        <div className="caption">
-          <h4 className="greetings">Hi</h4>
-          {/* <h1>I AM PRATEEK JENA,</h1> */}
-          <h2>I am a Web Developer</h2><br/>
-          {/* <h2>Web Developer</h2> */}
-          {/* <img style={{ width: "15rem" }} src="/assets/spddnb.gif" alt="spidey"/> */}
-        </div>
+    // Set interval to run the printRole function every 9 second 9000 milliseconds)
+    setInterval(printRole, 9000);
+  };
+  return (
+    <div className="intro" style={{ height: props.availHeight }}>
+      <Snowfall />
+      <div className="introImage" style={{ height: props.availHeight }} />
+      <div className="caption">
+        <h4 className="greetings">Hi</h4>
+        <h2 key={intro}>I am a {intro}</h2>  {/** key has been added so that react will understand there is a new node and rerender the css! */}
+        <br />
       </div>
-      </>
-    );
-  }
-}
+    </div>
+  );
+};
