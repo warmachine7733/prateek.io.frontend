8697582badd544fcbd5ec827210c499aea02098c
Prateek kumar Jena <32098667+warmachine7733@users.noreply.github.com>
2024-12-22 12:35:49 +0530

    address

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -28,7 +28,7 @@ export const About = ({aboutText}) => {
                 </td>
                 <td>
                   <i className="fa fa-map-marker" aria-hidden="true"></i>
-                  <Text>Betnoti,Odisha,India</Text>
+                  <TextIndia</Text>
                 </td>
               </tr>
               <tr className="tableRow">
