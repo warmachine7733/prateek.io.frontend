c5c0a26598a7729475aedff4b54cfc50368ddccb
warmachine7733 <prateekjena7733@gmail.com>
2025-11-15 21:31:01 +0530

    Revert "webpack: updates"
    This reverts commit 49b8027242692bc4375a63fa94902cde03560b28.

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,26 +1,15 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-
-const SRC_DIR = path.resolve("src");
 
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         loader: "babel-loader",
-        include: SRC_DIR,
         exclude: /node_modules/,
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: [require.resolve("react-refresh/babel")],
         },
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
-        include: SRC_DIR,
-        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
@@ -31,16 +20,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-      // favicon: "./public/favicon.ico",
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-      },
+      favicon: "./public/favicon.ico",
     }),
   ],
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: [".js", ".jsx"],
   },
 };
--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,22 +1,15 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = merge(common, {
   entry: "./src/index.js",
   mode: "development",
   devServer: {
-    historyApiFallback: true,
-    hot: true,
     port: 3000,
-    server: 'http',
-    static: {
-      directory: './public',
-    }
+    historyApiFallback: true,
   },
-  plugins: [new ReactRefreshWebpackPlugin()],
   output: {
-    publicPath: "/",
+    publicPath: "http://localhost:3000/",
     filename: "main.js",
   },
 });
