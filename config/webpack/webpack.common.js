49b8027242692bc4375a63fa94902cde03560b28
warmachine7733 <prateekjena7733@gmail.com>
2025-11-15 21:27:36 +0530

    webpack: updates

--- config/webpack/webpack.common.js
+++ config/webpack/webpack.common.js
@@ -1,15 +1,26 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const SRC_DIR = path.resolve("src");
 
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         loader: "babel-loader",
+        include: SRC_DIR,
         exclude: /node_modules/,
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: [require.resolve("react-refresh/babel")],
         },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        include: SRC_DIR,
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
@@ -20,10 +31,16 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
-      favicon: "./public/favicon.ico",
+      // favicon: "./public/favicon.ico",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
     }),
   ],
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
 };
--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -1,15 +1,22 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = merge(common, {
   entry: "./src/index.js",
   mode: "development",
   devServer: {
-    port: 3000,
     historyApiFallback: true,
+    hot: true,
+    port: 3000,
+    server: 'http',
+    static: {
+      directory: './public',
+    }
   },
+  plugins: [new ReactRefreshWebpackPlugin()],
   output: {
-    publicPath: "http://localhost:3000/",
+    publicPath: "/",
     filename: "main.js",
   },
 });
